b5831c8eb82899a0ec40cac5a489038efe367831
ShellTux <115948079+ShellTux@users.noreply.github.com>
2024-03-07 22:35:02 +0000

    fix(rke-generate-ssh-keys.sh): copy ssh keys to master machine

--- scripts/all/rke-generate-ssh-keys.sh
+++ scripts/all/rke-generate-ssh-keys.sh
@@ -2,32 +2,41 @@
 set -e
 
 usage() {
-	echo "Usage: $(basename "$0")"
+	echo "Usage: $(basename "$0") <master machine>"
+	echo
+	echo "Example: $(basename "$0") aida@10.3.1.150"
 	exit 1
 }
 
-[ "$#" -ne 0 ] && usage
+[ "$#" -lt 1 ] && usage
 
 user="$(whoami)"
 if [ -z "$user" ]
 then
 	echo "user is empty: $user" >&2
-	exit 1
+	usage
 fi
 
 hostname="$(hostname)"
 if [ -z "$hostname" ]
 then
 	echo "hostname is empty: $hostname" >&2
-	exit 1
+	usage
 fi
 
 type=ed25519
-file=~/.ssh/id_rsa
+ssh_file=~/.ssh/id_rsa
 password=
+master_machine="$1"
+if [ -z "$master_machine" ]
+then
+	echo "master_machine is empty: $master_machine" >&2
+	usage
+fi
 
 set -xe
 
-ssh-keygen -t "$type" -C "$user@$hostname" -f "$file" -N "$password"
+ssh-keygen -t "$type" -C "$user@$hostname" -f "$ssh_file" -N "$password"
 sed -i '/'"$user"'@'"$hostname"'/d' ~/.ssh/authorized_keys
-ssh-keygen -y -f "$file" | tee --append ~/.ssh/authorized_keys
+ssh-keygen -y -f "$ssh_file" | tee --append ~/.ssh/authorized_keys
+scp "$ssh_file" "$user":~/.ssh/"$hostname"
