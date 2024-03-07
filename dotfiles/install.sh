52f4a793408c9dd4b195153f72583b878a0844b0
Luís Góis <115948079+ShellTux@users.noreply.github.com>
2024-03-07 13:59:53 +0000

    Feat/dotfiles install script (#1)
    * add script
    
    * rename files
    
    * implement dotfiles installation for master and worker type machines
    
    * rename masterk8s -> master
    
    * fix(dotfiles/install.sh): fix file path
    
    * fix(dotfiles/install.sh): fix file

---
+++ dotfiles/install.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+set -e
+
+usage() {
+	echo "Usage: $(basename "$0") <master | worker>"
+	exit 1
+}
+
+[ "$#" -lt 1 ] && usage
+
+dotfiles_dir="$(dirname "$0")"
+
+installation() {
+	directory="$1"
+	find "$directory" -type f \
+		| while read -r filepath
+		do
+			file="$(basename "$filepath")"
+			install --mode=644 --verbose "$filepath" "$HOME/$file"
+		done
+}
+
+all() {
+	installation "$dotfiles_dir/all"
+}
+
+master() {
+	all
+	installation "$dotfiles_dir/master"
+}
+
+worker() {
+	all
+	installation "$dotfiles_dir/worker"
+}
+
+case "$1" in
+	master) master ;;
+	worker) worker ;;
+	*)      usage  ;;
+esac
