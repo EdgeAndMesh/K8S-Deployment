598a958fc2a55ca1676de43e2a1526fda031e80d
ShellTux <115948079+ShellTux@users.noreply.github.com>
2024-03-07 16:25:37 +0000

    feat(cri-dockerd-install.sh): install cri-dockerd and systemd service unit files

--- scripts/cri-dockerd-install.sh
+++ scripts/cri-dockerd-install.sh
@@ -4,5 +4,25 @@ set -xe
 version=0.3.10
 temp_dir="$(mktemp --directory)"
 cd "$temp_dir"
-curl -LO "https://github.com/Mirantis/cri-dockerd/releases/download/v$version/cri-dockerd-$version.amd64.tgz"
+
+set -x
+
+curl --silent --location --remote-name "https://github.com/Mirantis/cri-dockerd/releases/download/v$version/cri-dockerd-$version.amd64.tgz"
 tar -xvzf "cri-dockerd-$version.amd64.tgz"
+
+cd cri-dockerd
+sudo mkdir --parents /usr/local/bin
+sudo install \
+	--owner=root \
+	--group=root \
+	--mode=0755 \
+	cri-dockerd /usr/local/bin/cri-dockerd
+cd ..
+rm -r cri-dockerd
+
+git clone https://github.com/Mirantis/cri-dockerd.git
+cd cri-dockerd
+sudo install packaging/systemd/* /etc/systemd/system
+sudo sed -i -e 's,/usr/bin/cri-dockerd,/usr/local/bin/cri-dockerd,' /etc/systemd/system/cri-docker.service
+sudo systemctl daemon-reload
+sudo systemctl enable --now cri-docker.socket
