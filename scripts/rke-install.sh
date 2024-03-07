ca17e3405684f1c3f557c96f07b908e2ea69229f
Luís Góis <115948079+ShellTux@users.noreply.github.com>
2024-03-07 21:24:43 +0000

    Feat/rke install script (#3)
    * add rke-install.sh script
    
    * fix(rke-install.sh): download with root priviliges and rm file if sha256 checksum fails
    
    * fix(rke-install.sh): add execute permissions to binary

---
+++ scripts/rke-install.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+set -e
+
+arch=rke_linux-amd64
+version=1.4.15
+url_binary="https://github.com/rancher/rke/releases/download/v$version/$arch"
+url_sha256="https://github.com/rancher/rke/releases/download/v$version/sha256sum.txt"
+file=/usr/local/bin/rke
+
+set -xe
+sudo curl --location --silent --output "$file" "$url_binary"
+sudo chmod +x "$file"
+curl --location --silent "$url_sha256" \
+	| awk '/'"$arch"'/ { print $1 "  '"$file"'" }' \
+	| sha256sum --check \
+	|| sudo rm "$file"
