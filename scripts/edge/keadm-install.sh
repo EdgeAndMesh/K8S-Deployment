b986fa9634c0d3021abd4bf1933b41cb78d43fa9
ShellTux <115948079+ShellTux@users.noreply.github.com>
2024-03-22 15:20:08 +0000

    feat(keadm-install.sh): remove all files if checksum fails

--- scripts/edge/keadm-install.sh
+++ scripts/edge/keadm-install.sh
@@ -9,12 +9,19 @@ temp_dir="$(mktemp --directory)"
 
 cd "$temp_dir"
 
-binary_url="https://github.com/kubeedge/kubeedge/releases/download/v$version/keadm-v$version-linux-$arch.tar.gz"
+archive_url="https://github.com/kubeedge/kubeedge/releases/download/v$version/keadm-v$version-linux-$arch.tar.gz"
 checksum_url="https://github.com/kubeedge/kubeedge/releases/download/v$version/checksum_keadm-v$version-linux-$arch.tar.gz.txt"
 
-curl --location --remote-name "$binary_url"
-tar -zxvf "keadm-v$version-linux-$arch.tar.gz"
-
+curl --location --remote-name "$archive_url"
 curl --location --remote-name "$checksum_url"
+
+if ! awk '{print $0 " keadm-v'"$version"'-linux-'"$arch"'.tar.gz"}' "checksum_keadm-v$version-linux-$arch.tar.gz.txt" | sha512sum --check
+then
+	cd
+	rm -rdf "$temp_dir"
+	exit 1
+fi
+
+tar -zxvf "keadm-v$version-linux-$arch.tar.gz"
 
 sudo install --owner=root --group=root "keadm-v$version-linux-$arch/keadm/keadm" "$install_dir/keadm"
